462992440ab63c99235ad4f4be9d7087c5d9d61f
Darshna111 <144988893+Darshna111@users.noreply.github.com>
2024-02-15 18:35:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        "primary1": "#aacbad"
+      },
+      borderRadius: {
+        "xl": "50px"
+      }
+
+    },
+  },
+  plugins: [],
+}
